540022ae1d4a746fb68cb4cd4a2357c24efe852f
jeffminsungkim <jeffminsungkim@gmail.com>
2020-01-27 13:19:34 +0900

    test: fix e2e tests

--- tests/src/app.e2e-spec.ts
+++ tests/src/app.e2e-spec.ts
@@ -2,6 +2,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { uid } from '../fixtures/uid';
+import {
+  IMAGE_UPLOAD_MODULE_BASE_PATH,
+  USER_PROFILE_IMAGE_UPLOAD_MODULE_BASE_PATH,
+} from '../fixtures/base-path.constants';
 import path from 'path';
 import request from 'supertest';
 
@@ -21,39 +25,62 @@ describe('AppModule', () => {
     await app.close();
   });
 
-  it('ImageUploadController /POST without-dynamic-key-path', async () => {
-    return request(app.getHttpServer())
-      .post(`/image-upload/without-dynamic-key-path`)
-      .set('Content-Type', 'multipart/form-data')
-      .attach('file', path.resolve(__dirname, 'images/smile.jpg'))
-      .expect(201)
-      .expect(({ body }) => body.key === `${process.env.AWS_S3_APP_NAME}/smile.jpg`);
+  describe('ImageUploadController /POST', () => {
+    const basePath = `${IMAGE_UPLOAD_MODULE_BASE_PATH}`;
+    const dynamicPath = `${IMAGE_UPLOAD_MODULE_BASE_PATH}/${uid}`;
+
+    it(`should upload an image under the base path "${basePath}"`, async () => {
+      const res = await request(app.getHttpServer())
+        .post(`/image-upload/without-dynamic-key-path`)
+        .set('Content-Type', 'multipart/form-data')
+        .attach('file', path.resolve(__dirname, 'data/smile.jpg'));
+
+      expect(res.status).toEqual(201);
+      expect(res.body.key).toEqual(`${basePath}/smile.jpg`);
+    });
+
+    it(`should upload an image under the dynamic path "${dynamicPath}"`, async () => {
+      const res = await request(app.getHttpServer())
+        .post(`/image-upload/with-dynamic-key-path`)
+        .set('Content-Type', 'multipart/form-data')
+        .attach('file', path.resolve(__dirname, 'data/smile.jpg'));
+
+      expect(res.status).toEqual(201);
+      expect(res.body.key).toEqual(`${dynamicPath}/smile.jpg`);
+    });
+
+    it(`should not upload non image format`, async () => {
+      const res = await request(app.getHttpServer())
+        .post(`/image-upload/non-image-file`)
+        .set('Content-Type', 'multipart/form-data')
+        .attach('file', path.resolve(__dirname, 'data/Readme.md'));
+
+      expect(res.status).toEqual(400);
+    });
   });
 
-  it('ImageUploadController /POST with-dynamic-key-path', async () => {
-    return request(app.getHttpServer())
-      .post(`/image-upload/with-dynamic-key-path`)
-      .set('Content-Type', 'multipart/form-data')
-      .attach('file', path.resolve(__dirname, 'images/smile.jpg'))
-      .expect(201)
-      .expect(({ body }) => body.key === `${process.env.AWS_S3_APP_NAME}/${uid}/smile.jpg`);
-  });
+  describe('UserProfileImageUploadController /POST', () => {
+    const basePath = `${USER_PROFILE_IMAGE_UPLOAD_MODULE_BASE_PATH}`;
+    const dynamicPath = `${USER_PROFILE_IMAGE_UPLOAD_MODULE_BASE_PATH}/${uid}`;
 
-  it('UserProfileImageUploadController /POST without-dynamic-key-path', async () => {
-    return request(app.getHttpServer())
-      .post(`/user-profile-image-upload/without-dynamic-key-path`)
-      .set('Content-Type', 'multipart/form-data')
-      .attach('file', path.resolve(__dirname, 'images/crying.jpg'))
-      .expect(201)
-      .expect(({ body }) => body.key === `${process.env.AWS_S3_APP_NAME}/crying.jpg`);
-  });
+    it(`should upload an image under the base path "${basePath}"`, async () => {
+      const res = await request(app.getHttpServer())
+        .post(`/user-profile-image-upload/without-dynamic-key-path`)
+        .set('Content-Type', 'multipart/form-data')
+        .attach('file', path.resolve(__dirname, 'data/crying.jpg'));
 
-  it('UserProfileImageUploadController /POST with-dynamic-key-path', async () => {
-    return request(app.getHttpServer())
-      .post(`/user-profile-image-upload/with-dynamic-key-path`)
-      .set('Content-Type', 'multipart/form-data')
-      .attach('file', path.resolve(__dirname, 'images/crying.jpg'))
-      .expect(201)
-      .expect(({ body }) => body.key === `${process.env.AWS_S3_APP_NAME}/${uid}/crying.jpg`);
+      expect(res.status).toEqual(201);
+      expect(res.body.key).toEqual(`${basePath}/crying.jpg`);
+    });
+
+    it(`should upload an image under the dynamic path "${dynamicPath}"`, async () => {
+      const res = await request(app.getHttpServer())
+        .post(`/user-profile-image-upload/with-dynamic-key-path`)
+        .set('Content-Type', 'multipart/form-data')
+        .attach('file', path.resolve(__dirname, 'data/crying.jpg'));
+
+      expect(res.status).toEqual(201);
+      expect(res.body.key).toEqual(`${dynamicPath}/crying.jpg`);
+    });
   });
 });
