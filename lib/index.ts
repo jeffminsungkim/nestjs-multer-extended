7d2462ab774429a20c7c7c7c27d6d152f7db2eae
jeffminsungkim <jeffminsungkim@gmail.com>
2020-01-29 11:12:32 +0900

    refactor: export interceptors and the module

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,5 @@
 export * from './multer-config.service';
-export * from './multer-extended-core.module';
+export * from './multer-extended.module';
+export * from './interceptors';
 export * from './interfaces';
 export * from './constants';
